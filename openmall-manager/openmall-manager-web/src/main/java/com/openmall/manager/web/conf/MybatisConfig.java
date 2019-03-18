cf0af1bb13a3469719429824f16cb5266701b844
huangguozhong <505459193@qq.com>
2019-03-18 17:10:47 +0800

    整合来druid后台监控访问路径/druid 账号admin 密码123456

--- openmall-manager/openmall-manager-web/src/main/java/com/openmall/manager/web/conf/MybatisConfig.java
+++ openmall-manager/openmall-manager-web/src/main/java/com/openmall/manager/web/conf/MybatisConfig.java
@@ -5,7 +5,6 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -22,15 +21,12 @@ public class MybatisConfig {
     @Value("${mybatis.config-location}")
     private String configLocation;
 
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource(){return new com.alibaba.druid.pool.DruidDataSource();
-    }
+   
 
     @Bean(name="sqlSessionFactory")
-    public SqlSessionFactory sqlSessionFactory() throws  Exception{
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws  Exception{
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource());
+        sqlSessionFactoryBean.setDataSource(dataSource);
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         sqlSessionFactoryBean.setMapperLocations(resolver.getResources(mapperLocationPattern));
         sqlSessionFactoryBean.setConfigLocation(resolver.getResource(configLocation));

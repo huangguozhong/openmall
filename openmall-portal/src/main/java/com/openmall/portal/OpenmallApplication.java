5baba47098106fee682b67ea2fa1b46782cebc79
huangguozhong <505459193@qq.com>
2019-03-24 13:08:49 +0800

    portal门户模块springboot的war包部署方式

---
+++ openmall-portal/src/main/java/com/openmall/portal/OpenmallApplication.java
@@ -0,0 +1,13 @@
+package com.openmall.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenmallApplication.class, args);
+	}
+
+}
---
+++ openmall-portal/src/main/java/com/openmall/portal/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.openmall.portal;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OpenmallApplication.class);
+	}
+
+}
